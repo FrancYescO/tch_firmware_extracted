e3a9a173285f3408057a067b86037ab91ef19357
Francesco Molitierno <franc.molitierno@gmail.com>
2020-03-02 23:19:08 +0100

    AGTHP_2.1.0_CLOSED

---
+++ usr/lib/lua/mobiled/version.lua
@@ -0,0 +1,12 @@
+---------------------------------
+--! @file
+--! @brief Provides functionality to read out the current Mobiled version
+---------------------------------
+
+local M = {}
+
+function M.version()
+	return "0.6-361006bc24b6abe79043489583085c9585af1d47"
+end
+
+return M
