76f584c84386abf8d97c903c9a5d5f962cedd1d4
Francesco Molitierno <franc.molitierno@gmail.com>
2020-03-02 22:46:33 +0100

    AGTHP_1.1.0_001_CLOSED

---
+++ usr/lib/lua/mobiled/version.lua
@@ -0,0 +1,12 @@
+---------------------------------
+--! @file
+--! @brief Provides functionality to read out the current Mobiled version
+---------------------------------
+
+local M = {}
+
+function M.version()
+	return "0.6-23bc3ad0bb8db9d8c69dae05edf34423dbd11b16"
+end
+
+return M
---
+++ usr/share/ngwfdd/mod_slabinfo.lua
@@ -0,0 +1,59 @@
+#! /usr/bin/env lua
+
+-- file: mod_slabinfo.lua
+
+package.path = "/usr/share/ngwfdd/lib/?.lua;" .. package.path
+
+local gwfd = require("gwfd-common")
+
+-- Uloop and logger
+
+local uloop = require("uloop")
+
+-- Uploop timer
+
+local timer
+
+-- Get interval from UCI
+local interval = (tonumber(gwfd.get_uci_param("ngwfdd.interval.slabinfo")) or 300) * 1000
+
+-- Absolute path to the output fifo file
+
+local fifo_file_path = arg[1]
+local regex = "^([^%s]+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+(%d+)%s+:.*:%s+slabdata%s+%d+%s+(%d+)%s+%d+"
+
+local function send_slabinfo()
+  local file = io.open("/proc/slabinfo", "r")
+
+  local line = file:read("*l")
+  while line do
+    local name, nr_active_objs, nr_objs, obj_size, objs_per_slab, pages_per_slab, nr_slabs  = line:match(regex)
+    if name and nr_active_objs and nr_objs and obj_size
+       and objs_per_slab and pages_per_slab and nr_slabs then
+      local msg = {}
+      msg.name = name
+      msg.objects = nr_objs
+      msg.active_objects = nr_active_objs
+      msg.object_size = obj_size
+      msg.objects_per_slab = objs_per_slab
+      msg.pages_per_slab = pages_per_slab
+      msg.slabs = nr_slabs
+      gwfd.write_msg_to_file(msg, fifo_file_path)
+    end
+    line = file:read("*l")
+  end
+
+  file:close()
+
+  timer:set(interval) -- reschedule on the uloop
+end
+
+
+-- Main code
+uloop.init()
+
+gwfd.init("gwfd_slabinfo", 6, { init_transformer = true })
+
+timer = uloop.timer(send_slabinfo)
+send_slabinfo()
+xpcall(uloop.run, gwfd.errorhandler)
