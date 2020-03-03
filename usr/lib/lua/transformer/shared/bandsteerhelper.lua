166319e71a4505774e081fba36fabff1d4d3e006
Francesco Molitierno <franc.molitierno@gmail.com>
2020-03-03 02:11:51 +0100

    vbnt-v_CRF979_18.1.c.0443-950-RA

---
+++ usr/lib/lua/transformer/shared/bandsteerhelper.lua
@@ -0,0 +1,289 @@
+local M = {}
+
+local uci_helper = require("transformer.mapper.ucihelper")
+local ubus = require("ubus")
+local binding_wireless = {config = "wireless"}
+local conn = ubus.connect()
+local strmatch, format = string.match, string.format
+local network = require("transformer.shared.common.network")
+
+function M.isBaseIface(iface)
+  return "0" == strmatch(iface, "%d+")
+end
+
+function M.getBsAp(iface)
+  local data = conn:call("wireless.accesspoint", "get",  {})
+  for k, v in pairs(data) do
+    if v.ssid == iface then
+      return k, v
+    end
+  end
+  return nil
+end
+
+local function getAllSSID()
+  local data = conn:call("wireless.ssid", "get",  { })
+    if data == nil then
+      return {}
+    end
+  local entries = {}
+  for k in pairs(data) do
+    entries[#entries + 1] = k
+  end
+  return entries
+end
+
+local function getWirelessUciValue(sectionName, option)
+  binding_wireless.sectionname = sectionName
+  binding_wireless.option = option
+  return uci_helper.get_from_uci(binding_wireless)
+end
+
+local function setWirelessUciValue(value, sectionName, option)
+  binding_wireless.sectionname = sectionName
+  binding_wireless.option = option
+  uci_helper.set_on_uci(binding_wireless, value, commitapply)
+end
+
+function M.getBandSteerPeerIface(key)
+  local ssidData = getAllSSID()
+  if ssidData and next(ssidData) then
+    local tmpstr = strmatch(key, ".*_(%d+)")
+    for _, v in pairs(ssidData) do
+      if v ~= key then
+          if not tmpstr then
+            if not strmatch(v, ".*_(%d+)") then
+              return v
+            end
+          else
+            if tmpstr == strmatch(v, ".*_(%d+)") then
+              return v
+            end
+          end
+      end
+    end
+  end
+
+  return nil, "To get band steer switching SSID failed."
+end
+
+function M.isBandSteerSectionConfigured(bandsteerID)
+  local data = conn:call("wireless.bandsteer", "get", {})
+  if not data then
+      return false, "Please configure band steer section " .. bandsteerID
+  end
+
+  for k in pairs(data) do
+      if k == bandsteerID then
+          return true
+      end
+  end
+
+  return false, "Please configure band steer section " .. bandsteerID
+end
+
+function M.getBandSteerId(iface)
+  local tmpstr = strmatch(iface, ".*_(%d+)")
+  local bsID
+  if not tmpstr then
+      bsID = format("%s", "bs0")
+  else
+      bsID = format("%s", "bs" .. tmpstr)
+  end
+
+  --to judge whether the section configed or not
+  local ret, errmsg = M.isBandSteerSectionConfigured(bsID)
+  if not ret then
+      return nil, errmsg
+  end
+
+  return bsID
+end
+
+function M.getApBandSteerId(ap)
+  return getWirelessUciValue(ap, "bandsteer_id")
+end
+
+function M.isBandSteerEnabledByAp(ap)
+  local bandsteerid = M.getApBandSteerId(ap)
+  if bandsteerid and "" ~= bandsteerid and "off" ~= bandsteerid then
+      return true
+  end
+  return false
+end
+
+--For 5G when bandsteer enabled, the ssid and authentication related option cannot be modified
+function M.isBandSteerEnabledByIface(iface)
+  local ap = M.getBsAp(iface)
+  if type(ap) == 'string' then
+      return M.isBandSteerEnabledByAp(ap)
+  end
+
+  return false
+end
+
+function M.canEnableBandSteer(apKey, apData, iface)
+  if type(apKey) ~= 'string' or type(iface) ~= 'string' then
+    return false, "Ap or Iface is invalid."
+  end
+
+  if not apData or not next(apData) or "1" ~= tostring(apData.admin_state) then
+    return false, "Please enable network firstly."
+  end
+
+  local bandsteerID = M.getApBandSteerId(apKey)
+  if bandsteerID and "" ~= bandsteerID and "off" ~= bandsteerID then
+    return false, "Band steering has already been enabled."
+  end
+
+  if getWirelessUciValue(apKey, "security_mode") == "wep" then
+    return false, "Band steering cannot be supported in wep mode."
+  end
+
+  local peerIface, errmsg = M.getBandSteerPeerIface(iface)
+  if not peerIface then
+    return false, errmsg
+  end
+
+  local peerAP, peerAPNode = M.getBsAp(peerIface)
+  if not peerAP then
+    return false, "Band steering switching node does not exist."
+  end
+
+  if tostring(peerAPNode.admin_state) ~= "1" then
+    return false, "Please enable network for band steering switching node firstly."
+  end
+
+  if getWirelessUciValue(peerAP, "security_mode") == "wep" then
+    return false, "Band steering cannot be supported in wep mode."
+  end
+
+  return true
+end
+
+function M.canDisableBandSteer(apKey, iface)
+  if type(apKey) ~= 'string' then
+    return false, "Ap is invalid."
+  end
+
+  local bandsteerid = M.getApBandSteerId(apKey)
+  if not bandsteerid or "" == bandsteerid or "off" == bandsteerid then
+    return false, "Band steering has already been disabled."
+  end
+
+  local peerIface = M.getBandSteerPeerIface(iface)
+  if not peerIface then
+    return false, "Band steering switching node does not exist."
+  end
+
+  local peerAP = M.getBsAp(peerIface)
+  if not peerAP then
+    return false, "Band steering switching node does not exist."
+  end
+
+  return true
+end
+
+function M.setBandSteerPeerIfaceSSIDByLocalIface(baseiface, needsetiface, oper)
+  if "1" == oper then
+    --to get the baseiface ssid
+    local baseifacessid = getWirelessUciValue(baseiface, "ssid")
+
+    if "" ~= baseifacessid then
+      setWirelessUciValue(baseifacessid, needsetiface, "ssid")
+    end
+  else
+    setWirelessUciValue(getWirelessUciValue(needsetiface, "ssid") .. "-5G", needsetiface, "ssid")
+  end
+end
+
+function M.setBandSteerPeerIfaceSSIDValue(needsetiface, value)
+  setWirelessUciValue(value, needsetiface, "ssid")
+end
+
+local function getBandSteerRelatedNode(apKey, apNode)
+  local peerIface, errmsg = M.getBandSteerPeerIface(apNode.ssid)
+  if not peerIface then
+    return nil, errmsg
+  end
+
+  local bspeerap = M.getBsAp(peerIface)
+  if not bspeerap then
+    return nil, "Band steering switching node does not exist"
+  end
+
+  if M.isBaseIface(apNode.ssid) then
+    return apKey, bspeerap, apNode.ssid, peerIface
+  else
+    return bspeerap, apKey, peerIface, apNode.ssid
+  end
+end
+
+--to set the authentication related content
+local function setBandSteerPeerApAuthentication(baseap, needsetap)
+  local value = getWirelessUciValue(baseap, "security_mode")
+  setWirelessUciValue(value, needsetap, "security_mode")
+  value = getWirelessUciValue(baseap, "wpa_psk_key")
+  setWirelessUciValue(value, needsetap, "wpa_psk_key")
+end
+
+local function setBandSteerID(ap, bspeerap, bsid)
+  setWirelessUciValue(bsid, ap, "bandsteer_id")
+  setWirelessUciValue(bsid, bspeerap, "bandsteer_id")
+end
+
+local function disableBandSteer(key)
+  local apData = network.getAccessPointInfo(key)
+  if not apData or not next(apData) then
+    return nil, "The related AP node cannot be found."
+  end
+
+  local ret, errmsg = M.canDisableBandSteer(key, apData.ssid)
+  if not ret then
+    return nil, errmsg
+  end
+
+  local baseap, needsetap, baseiface, needsetiface = getBandSteerRelatedNode(key, apData)
+  setBandSteerID(baseap, needsetap, "off")
+
+  --to reset the ssid
+  M.setBandSteerPeerIfaceSSIDByLocalIface(baseiface, needsetiface, "0")
+end
+
+--1\Only the admin_state enabled, then enable bandsteering
+--2\2.4G related ap will act as based node
+local function enableBandSteer(key)
+  local apNode = network.getAccessPointInfo(key)
+  if not apNode then
+    return nil, "AP node is invalid."
+  end
+
+  local ret, errmsg = M.canEnableBandSteer(key, apNode, apNode.ssid)
+  if not ret then
+    return nil, errmsg
+  end
+  --to set the bandsteer ids
+  local baseap, needsetap, baseiface, needsetiface = getBandSteerRelatedNode(key, apNode)
+  local bsid, errorMsg = M.getBandSteerId(apNode.ssid)
+  if not bsid then
+    return nil, errorMsg
+  end
+  setBandSteerID(baseap, needsetap, bsid)
+  M.setBandSteerPeerIfaceSSIDByLocalIface(baseiface, needsetiface, "1")
+  setBandSteerPeerApAuthentication(baseap, needsetap)
+end
+
+function M.setBandSteerValue(value, key)
+  local bandSteer, errMsg
+  if value == "1" then
+    bandSteer, errMsg = enableBandSteer(key)
+  else
+    bandSteer, errMsg = disableBandSteer(key)
+  end
+  if not bandSteer then
+    return nil, errMsg
+  end
+  return bandSteer
+end
+
+return M
---
+++ usr/share/ngwfdd/mod_cpustats.lua
@@ -0,0 +1,99 @@
+#! /usr/bin/env lua
+
+-- file: mod_cpustats.lua
+
+package.path = "/usr/share/ngwfdd/lib/?.lua;" .. package.path
+
+local gwfd = require("gwfd-common")
+local uloop = require("uloop")
+
+-- Logger
+
+local log
+
+-- Uloop timer
+
+local timer
+local interval = (tonumber(gwfd.get_uci_param("ngwfdd.interval.cpustats")) or 600) * 1000
+
+-- Absolute path to the output fifo file
+
+local fifo_file_path = arg[1]
+
+-- CPU statistics
+
+local cpu_stats
+
+local function parse_proc_stat_entry(l)
+  local result = {}
+
+  local work = 0
+  local total = 0
+  local i = 1
+
+  for token in l:gmatch("([^ ]+)") do
+    -- only first three fields (user/nice/system) are counted as effective usage
+    if (i < 4) then
+      work = work + token
+    end
+    total = total + token
+    i = i + 1
+  end
+  result["work"] = work
+  result["total"] = total
+  return result
+end
+
+local function parse_proc_stat_file()
+  local result = {}
+
+  local file = io.open("/proc/stat", "r")
+  local line = file:read("*l")
+
+  local cpu_exp = "cpu[0-9]*"
+
+  while line do
+    local cpu = line:match(cpu_exp)
+    if cpu then
+      local stats = string.sub(line, string.len(cpu) + 1)
+      local sums = parse_proc_stat_entry(stats)
+      result[cpu] = sums
+    end
+    line = file:read("*l")
+  end
+
+  file:close()
+  return result
+end
+
+local function get_cpu_params(t1, msg)
+  local t2 = parse_proc_stat_file()
+
+  for cpu, s in pairs(t1) do
+    local usage = (t2[cpu]["work"] - s["work"]) / (t2[cpu]["total"] - s["total"]) * 100
+    msg[cpu] = usage
+  end
+
+  return t2
+end
+
+local function send_cpustats_data()
+  local msg = {}
+  local new_cpu_stats = get_cpu_params(cpu_stats, msg)
+  cpu_stats = new_cpu_stats
+
+  gwfd.write_msg_to_file(msg, fifo_file_path)
+  timer:set(interval) -- reschedule on the uloop
+end
+
+-- Main code
+uloop.init()
+
+log = gwfd.init("gwfd_cpustats", 6, { init_transformer = true })
+
+cpu_stats = parse_proc_stat_file()
+
+timer = uloop.timer(send_cpustats_data)
+timer:set(interval)
+
+xpcall(uloop.run, gwfd.errorhandler)
