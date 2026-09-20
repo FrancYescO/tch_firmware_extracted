ced03d94f93cae2d99d0bd525bc7b6b19dcfab9d
Francesco Molitierno <franc.molitierno@gmail.com>
2026-09-20 23:45:34 +0200

    vbnt-j_CRF847-17.2.0261-820-RA

---
+++ usr/lib/cwmpd/transfers/switchover.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+LOCK=/tmp/switchover.lock
+WAITFILE=/usr/lib/cwmpd/transfers/cwmp_waitforswitchover
+
+. $(dirname $0)/rollback_common.sh
+
+# create a lock dir or exit if it already exists
+mkdir $LOCK || exit
+
+if [ -f $WAITFILE ]; then
+	rollback_set_initiator DELAYED
+fi
+# if no switchover waiting, switch banks anyway.
+
+# wait for any voice calls or ongoing VOD to terminate
+lua /usr/lib/cwmpd/transfers/checkOngoingServices.lua
+
+# switch
+cat /proc/banktable/notbooted >/proc/banktable/active
+rm -f $WAITFILE
+
+# no need to remove the lock as it is in ram
+# and deleting introduces a race condition !!
+
+if [ -f /lib/functions/reboot_reason.sh ]; then
+	. /lib/functions/reboot_reason.sh
+	set_reboot_reason SWOVER
+fi
+reboot
