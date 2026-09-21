8e7d10ba1943a0b484c2bb05fdda5eff09778079
Francesco Molitierno <franc.molitierno@gmail.com>
2026-09-21 09:08:50 +0200

    AGTEF_2.0.1_CLOSED

---
+++ usr/lib/lua/mobiled/plugin.lua
@@ -0,0 +1,35 @@
+---------------------------------
+--! @file
+--! @brief The Plugin class loading the correct library
+---------------------------------
+
+local Plugin = {}
+Plugin.__index = Plugin
+
+function Plugin.create(runtime, name, cfg)
+    local p = {}
+    
+    local status, error = pcall(require, name)
+    p.plugin = status and error or nil
+    if not p.plugin then
+        return nil, "Failed to load " .. name .." plugin".." (" .. error .. ")"
+    end
+
+    p.name = name
+
+    status, error = p.plugin.init_plugin(runtime, cfg)
+    if not status then return nil, error end
+
+    setmetatable(p, Plugin)
+    return p
+end
+
+function Plugin:reconfigure(cfg)
+    return self.plugin.reconfigure_plugin(cfg)
+end
+
+function Plugin:destroy()
+    return self.plugin.destroy_plugin()
+end
+
+return Plugin
